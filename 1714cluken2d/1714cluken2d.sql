96e7f0b720f0257e5d937c1674b88152de54a9d3
Callum Luken <gm4134oe@go.minnstate.edu>
2020-10-07 16:27:44 -0500

    Completed Exercise 2d

---
+++ 1714cluken2d/1714cluken2d.sql
@@ -0,0 +1,65 @@
+-- Callum Luken
+-- Exercise 2D Summary Multi-table Join Queries
+-- Property Manager Database
+
+-- 2D.1 Buildings, Apartments, Administrators
+SELECT        Building.BuildingId, Building.BuildingName, Building.City, Apartment.ApartmentNum, Person.FirstName, Person.LastName
+FROM            Building INNER JOIN
+                         Apartment ON Building.BuildingId = Apartment.BuildingId INNER JOIN
+                         Person ON Apartment.AdminId = Person.PersonId
+GROUP BY Building.BuildingId, Building.BuildingName, Building.City, Apartment.ApartmentNum, Person.FirstName, Person.LastName
+ORDER BY Building.City, Building.BuildingName, Apartment.ApartmentNum, Person.LastName, Person.FirstName
+
+-- 2D.2 Tenants, Buildings, Apartments
+SELECT        Person.PersonId, Person.LastName, Person.FirstName, Building.City, Building.BuildingId, Building.BuildingName, Apartment.ApartmentNum
+FROM            Person INNER JOIN
+                         Apartment ON Person.PersonId = Apartment.TenantId INNER JOIN
+                         Building ON Apartment.BuildingId = Building.BuildingId
+GROUP BY Person.PersonId, Person.LastName, Person.FirstName, Building.City, Building.BuildingId, Building.BuildingName, Apartment.ApartmentNum
+ORDER BY Person.LastName, Person.FirstName, Building.BuildingId, Apartment.ApartmentNum
+
+-- 2D.3 Apartment, Tenant, Invoice, Line Items
+SELECT        Apartment.ApartmentNum, Person.LastName, Person.FirstName, Invoice.InvoiceId, Invoice.InvoiceDate, LineItem.Description, LineItem.Amount
+FROM            Apartment INNER JOIN
+                         Person ON Apartment.TenantId = Person.PersonId INNER JOIN
+                         Invoice ON Apartment.ApartmentId = Invoice.ApartmentId INNER JOIN
+                         LineItem ON Invoice.InvoiceId = LineItem.InvoiceId
+WHERE        (Apartment.BuildingId = 1)
+ORDER BY Apartment.ApartmentNum, Person.LastName, Person.FirstName, Invoice.InvoiceDate
+
+-- 2D.4 Apartment, Tenant, Invoice Total
+SELECT        Apartment.BuildingId, Apartment.ApartmentId, Person.LastName, Person.FirstName, Invoice.InvoiceId, Invoice.InvoiceDate, SUM(LineItem.Amount) AS Expr1
+FROM            Apartment INNER JOIN
+                         Person ON Apartment.TenantId = Person.PersonId INNER JOIN
+                         Invoice ON Apartment.ApartmentId = Invoice.ApartmentId INNER JOIN
+                         LineItem ON Invoice.InvoiceId = LineItem.InvoiceId
+GROUP BY Apartment.BuildingId, Apartment.ApartmentId, Person.LastName, Person.FirstName, Invoice.InvoiceId, Invoice.InvoiceDate
+
+-- 2D.5 Invoice, Tenant, Receipt
+SELECT        Invoice.InvoiceId, Invoice.InvoiceDate, Building.BuildingId, Apartment.ApartmentNum, Person.LastName, Person.FirstName, Receipt.ReceiptDate, Receipt.Amount
+FROM            Invoice INNER JOIN
+                         Apartment ON Invoice.ApartmentId = Apartment.ApartmentId INNER JOIN
+                         Building ON Apartment.BuildingId = Building.BuildingId INNER JOIN
+                         Person ON Apartment.TenantId = Person.PersonId INNER JOIN
+                         Receipt ON Invoice.InvoiceId = Receipt.InvoiceId
+ORDER BY Invoice.InvoiceDate, Receipt.ReceiptDate
+
+-- 2D.6 Invoice, Tenant, Apartment, Billed, Received
+SELECT        Invoice.InvoiceId, Invoice.InvoiceDate, Person.LastName, Building.BuildingId, Apartment.ApartmentNum, SUM(LineItem.Amount) AS [Total Billed], Receipt.Amount AS Received
+FROM            Invoice INNER JOIN
+                         Apartment ON Invoice.ApartmentId = Apartment.ApartmentId INNER JOIN
+                         Person ON Apartment.TenantId = Person.PersonId INNER JOIN
+                         Building ON Apartment.BuildingId = Building.BuildingId INNER JOIN
+                         Receipt ON Invoice.InvoiceId = Receipt.InvoiceId INNER JOIN
+                         LineItem ON Invoice.InvoiceId = LineItem.InvoiceId
+GROUP BY Invoice.InvoiceId, Invoice.InvoiceDate, Person.LastName, Building.BuildingId, Apartment.ApartmentNum, Receipt.Amount
+
+-- 2D.7
+SELECT        Person.PersonId, Person.LastName, Invoice.InvoiceId, Invoice.InvoiceDate, SUM(LineItem.Amount) AS Billed, Receipt.Amount AS Received
+FROM            Person INNER JOIN
+                         Apartment ON Person.PersonId = Apartment.AdminId INNER JOIN
+                         Invoice ON Apartment.ApartmentId = Invoice.ApartmentId INNER JOIN
+                         LineItem ON Invoice.InvoiceId = LineItem.InvoiceId INNER JOIN
+                         Receipt ON Invoice.InvoiceId = Receipt.InvoiceId
+GROUP BY Person.PersonId, Person.LastName, Invoice.InvoiceId, Invoice.InvoiceDate, Receipt.Amount
+ORDER BY Received
